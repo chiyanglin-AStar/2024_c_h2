41c1e16d240cdf49f9bef288aabd7fbe0a545816
chiyanglin <chiyanglin@gmail.com>
2024-10-24 20:37:56 +0800

    Create ex2-3.cpp

---
+++ ex2-3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{ 
+    printf("1:Hello World\n4:This sentence will work!\n\t5:This sentence will work!\n");
+    printf("2:This sentence will work!\n");
+    printf("3:This sentence will work!\n3:\\This sentence will work!\n3:\"This sentence will work!\n");
+    printf("4:This sentence will work!\n");
+    printf("5:This sentence will work!\n");
+    
+    return 0;
+    
+}
