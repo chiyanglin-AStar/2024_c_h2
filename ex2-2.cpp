c36ba3fc0041b20bd5f05e84b90abe5665dd1037
chiyanglin <chiyanglin@gmail.com>
2024-10-24 19:49:43 +0800

    Create ex2-2.cpp

---
+++ ex2-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{ 
+    printf("1:Hello World");
+    printf("2:This sentence will work!");
+    printf("3:This sentence will work!");
+    printf("4:This sentence will work!");
+    printf("5:This sentence will work!");
+    
+    return 0;
+    
+}
