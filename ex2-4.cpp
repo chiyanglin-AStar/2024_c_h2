9fb250434349a5a55aa7362c36d4558c6d1e5936
chiyanglin <chiyanglin@gmail.com>
2024-10-24 20:38:29 +0800

    Create ex2-4.cpp

---
+++ ex2-4.cpp
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+    
+    // this is comment by chiyang
+    printf("Hello World"); // this is comment 
+    // ------------------------
+    
+    return 0;
+}
